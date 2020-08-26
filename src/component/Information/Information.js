9f58183084cb8ebce03c2e423f130d16c84ee912
Tahsin Amin <Tahsin16@cse.pstu.ac.bd>
2020-08-27 05:37:08 +0600

    basic-react

--- src/component/Information/Information.js
+++ src/component/Information/Information.js
@@ -27,7 +27,7 @@ const Information = (props) => {
                    <p> <Badge variant="success">{feedback}</Badge>{' '} <FontAwesomeIcon icon={faUserFriends}></FontAwesomeIcon> Student : {student} <FontAwesomeIcon icon={faStar}></FontAwesomeIcon>Rating :{studentRating}</p>
                     <h5 className='author'>Crated by : {author}</h5>
                     <p><FontAwesomeIcon icon={faGlobe}></FontAwesomeIcon> { language}</p>
-                    <p className='price'>Price : <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon>{price}</p>
+                    <p className='price'>Price : <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon> {price} </p>
                    <Button onClick = {()=>{props.handleCourse(props.information);changeText('Enrolled')}} variant="outline-success"> <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon> {btnText} </Button>{' '}
                 </div>
             </div>
